8bbe98a3bde0e1aa7fffa7c48ad805d8922a6356
RomanKhal <haluev.roman@mail.ru>
2023-10-15 15:26:29 +0300

    finish step 3 of MindGames

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 public class Cli {
     private static String userName;
-    public static void getUsername(){
+    public static void setUserName(){
         System.out.print("May I Have Your Name? ");
         Scanner scanner = new Scanner(System.in);
         userName = scanner.nextLine();
-        System.out.printf("Hello, %s!", userName);
+        System.out.printf("Hello, %s!\n", userName);
+    }
+    public static String getUserName(){
+        return userName;
     }
 }
