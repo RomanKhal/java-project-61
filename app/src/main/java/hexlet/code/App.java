ea5a84405169f16030ec9ee491f8aaa95c5d8fce
RomanKhal <haluev.roman@mail.ru>
2023-10-24 18:47:15 +0300

    move chooseMenu() from Engine to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,61 @@
 package hexlet.code;
 
 
+import java.util.Scanner;
+
 public class App {
+    private static String gameNumber;
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Engine.chooseMenu();
+        chooseMenu();
     }
+
+    public static void chooseMenu() {
+        System.out.print("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit
+                Your choice:\s""");
+        try {
+            gameNumber = scanner.next();
+            if (!gameNumber.equals("0") && !gameNumber.equals("1")) {
+                Cli.setUserName();
+            }
+        } catch (Exception e) {
+            System.out.println("Choose a number please.");
+            chooseMenu();
+        }
+        switch (gameNumber) {
+            case "1":
+                Cli.setUserName();
+                break;
+            case "2":
+                Even.evenGame();
+                break;
+            case "3":
+                Calc.calcGame();
+                break;
+            case "4":
+                GCD.gcdGame();
+                break;
+            case "5":
+                Progression.progressionGame();
+                break;
+            case "6":
+                Prime.primeGame();
+                break;
+            case "0":
+                System.out.printf("Bye %s!\n", Cli.getUserName());
+                break;
+            default:
+                System.out.printf("%s is not an option.\n", gameNumber);
+        }
+    }
+
 }
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,62 +1,13 @@
 package hexlet.code;
 
-import java.util.Scanner;
 
 public class Engine {
     private static int winsCount = 0;
     public static final int WINS = 3;
-    private static String gameNumber;
 
-    public static void chooseMenu() {
-//        do {
-        System.out.print("""
-                Please enter the game number and press Enter.
-                1 - Greet
-                2 - Even
-                3 - Calc
-                4 - GCD
-                5 - Progression
-                6 - Prime
-                0 - Exit
-                Your choice:\s""");
-        try {
-            gameNumber = scanner.next();
-            if (!gameNumber.equals("0") && !gameNumber.equals("1")) {
-                Cli.setUserName();
-            }
-        } catch (Exception e) {
-            System.out.println("Choose a number please.");
-            chooseMenu();
-        }
-        switch (gameNumber) {
-            case "1":
-                Cli.setUserName();
-                break;
-            case "2":
-                Even.evenGame();
-                break;
-            case "3":
-                Calc.calcGame();
-                break;
-            case "4":
-                GCD.gcdGame();
-                break;
-            case "5":
-                Progression.progressionGame();
-                break;
-            case "6":
-                Prime.primeGame();
-                break;
-            case "0":
-                System.out.printf("Bye %s!\n", Cli.getUserName());
-                break;
-            default:
-                System.out.printf("%s is not an option.\n", gameNumber);
-        }
-//        } while (gameNumber != 0);
-    }
 
-    private static Scanner scanner = new Scanner(System.in);
+
+
     private static final String SCHEMA = """
             %s is wrong answer ;(. Correct answer was %s.
             Let's try again, %s!
